2750c09da12b6921d8b4003d4e7f2048aba37e4f
Ryo Suzuki <hapicky@users.noreply.github.com>
2023-11-26 21:54:33 +0900

    day15 part1を右と下だけでなく左と上にも移動するよう修正

--- 2021/day/15/part1.py
+++ 2021/day/15/part1.py
@@ -1,26 +1,47 @@
-from sys import stdin
+import sys
+
+sys.setrecursionlimit(10**9)
 
 cavern = []
 width = 0
 
-for line in stdin:
+for line in sys.stdin:
     width = len(line)
     cavern.append(line)
 
 height = len(cavern)
 
-# 上からy行目、左からx列目まで行く最小リスクをdp[y][x]とする
-dp = [[float('inf')] * (width+1) for _ in range(height+1)]
-dp[1][1] = 0
+# 上からy行目、左からx列目まで行く最小リスクをmin_risks[y-1][x-1]とする
+min_risks = [[float('inf')] * width for _ in range(height)]
+min_risks[0][0] = 0
 
-for y in range(1, height+1):
-    for x in range(1, width+1):
-        if y == 1 and x == 1:
-            continue
-        
-        dp[y][x] = min(dp[y-1][x], dp[y][x-1]) + int(cavern[y-1][x-1])
+def visit(y, x, cur_risk):
+    neighbors = []
 
-ans = dp[height][width]
+    # 右
+    if x < width - 1:
+        neighbors.append((y, x+1))
+
+    # 下
+    if y < height - 1:
+        neighbors.append((y+1, x))
+
+    # 左
+    if x > 1:
+        neighbors.append((y, x-1))
+    
+    # 上
+    if y > 1:
+        neighbors.append((y-1, x))
+    
+    for ny, nx in neighbors:
+        risk = int(cavern[ny][nx])
+        sum_risk = cur_risk + risk
+        if sum_risk < min_risks[ny][nx]:
+            min_risks[ny][nx] = sum_risk
+            visit(ny, nx, sum_risk)
+
+visit(0, 0, 0)
+
+ans = min_risks[height-1][width-1]
 print(ans)
-# なぜか正解と1違う値になったのだけど、なぜだろう？
-
