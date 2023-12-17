c2d3ba4c2724d8a4f0b0e474e0c819f6db0b9bd1
Ryo Suzuki <hapicky@users.noreply.github.com>
2023-12-18 00:12:17 +0900

    最少ロスを求めるのに全探索しか思いつかず、これだと遅すぎてサンプルも解けない

--- 2023/day/17/part1.py
+++ 2023/day/17/part1.py
@@ -1,5 +1,5 @@
 from sys import stdin
-import heapq
+from collections import defaultdict
 
 rows = []
 
@@ -10,21 +10,23 @@ for line in stdin:
 width = len(rows[0])
 height = len(rows)
 
+# min_loss[y][x] => (x, y)における最小ロス
 min_loss = [[float('inf')] * width for _ in range(height)]
 min_loss[0][0] = 0
-visited = [[False] * width for _ in range(height)]
-visited[0][0] =True
+# visited_start[(x, y)] => (x, y)を訪問済か否か
+visited_start = defaultdict(bool)
+visited_start[(0, 0)] = True
 
+# キュー：座標FROM, 座標TO, 現在ロス、方向、訪問済
 q = []
-heapq.heapify(q)
 
-heapq.heappush(q, ((0, 1), (0, 0), 0, 'down', visited))
-heapq.heappush(q, ((0, 2), (0, 0), 0, 'down', visited))
-# heapq.heappush(q, ((0, 3), (0, 0), 0, 'down', visited))
-heapq.heappush(q, ((1, 0), (0, 0), 0, 'right', visited))
-heapq.heappush(q, ((2, 0), (0, 0), 0, 'right', visited))
-# heapq.heappush(q, ((3, 0), (0, 0), 0, 'right', visited))
+# 最初のキューを登録
+q.append(((0, 0), (0, 1), 0, 'down', visited_start.copy()))
+q.append(((0, 0), (0, 2), 0, 'down', visited_start.copy()))
+q.append(((0, 0), (1, 0), 0, 'right', visited_start.copy()))
+q.append(((0, 0), (2, 0), 0, 'right', visited_start.copy()))
 
+# 各方向のx, y増分
 px_py = {
     'up': (0, -1),
     'down': (0, 1),
@@ -32,56 +34,51 @@ px_py = {
     'right': (1, 0),
 }
 
-def visit(point_to, point_from, cur_loss, dir, visited):
-    if dir == 'up':
-        dx = 0
-        dy = -1
+def visit(point_from, point_to, cur_loss, dir, visited):
+    # 方向のx, y増分
+    px, py = px_py[dir]
+
+    # 繰り返し数と次の方向
+    if dir == 'up' or dir == 'down':
         n = abs(point_from[1] - point_to[1])
         next_dirs = ['left', 'right']
-    elif dir == 'down':
-        dx = 0
-        dy = 1
-        n = abs(point_from[1] - point_to[1])
-        next_dirs = ['left', 'right']
-    elif dir == 'left':
-        dx = -1
-        dy = 0
+    else:
         n = abs(point_from[0] - point_to[0])
         next_dirs = ['up', 'down']
-    elif dir == 'right':
-        dx = 1
-        dy = 0
-        n = abs(point_from[0] - point_to[0])
-        next_dirs = ['up', 'down']
-    
+
     loss = cur_loss
     for i in range(n):
-        x = point_from[0] + dx * (i + 1)
-        y = point_from[1] + dy * (i + 1)
+        x = point_from[0] + px * (i + 1)
+        y = point_from[1] + py * (i + 1)
+        # ロスを加算
         loss += rows[y][x]
-    
+        # 訪問済を記録
+        visited[(x, y)] = True
+
+    # 最少ロスを更新
     min_loss[y][x] = min(min_loss[y][x], loss)
-    visited[point_to[0]][point_to[1]] = True
 
     for next_dir in next_dirs:
         px, py = px_py[next_dir]
         next_point_to = (point_to[0] + px, point_to[1] + py)
         for i in range(3):
             if next_point_to[0] < 0 or width <= next_point_to[0] or next_point_to[1] < 0 or height <= next_point_to[1]:
+                break
+
+            if visited[(next_point_to[0], next_point_to[1])] == True:
                 continue
 
-            if visited[next_point_to[0]][next_point_to[1]] == True:
-                continue
-
-            heapq.heappush(q, (next_point_to, point_to, loss, next_dir, visited))
+            q.append((point_to, next_point_to, loss, next_dir, visited.copy()))
             next_point_to = (next_point_to[0] + px, next_point_to[1] + py)
 
+cnt = 0
 while len(q) > 0:
-    point_to, point_from, cur_loss, dir, v = heapq.heappop(q)
-    visit(point_to, point_from, cur_loss, dir, v)
-
-for l in min_loss:
-    print(l)
-
+    point_from, point_to, cur_loss, dir, visited = q.pop(0)
+    visit(point_from, point_to, cur_loss, dir, visited)
+    cnt += 1
+    if cnt % 100 == 0:
+        for l in min_loss:
+            print(l)
+        print('---')
 
 print(min_loss[height-1][width-1])
